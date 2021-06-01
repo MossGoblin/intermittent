55f3646b43bc3a104b92c20d52d2b6e8876dfd75
Angel Slavchev <aslavchev@roiti.com>
2021-06-01 15:47:25 +0300

    Comparison

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from datetime import datetime
 from typing import List
 
 from crystal_sort import *
+from sort import *
 
+algo_list = [
+    'crystal',
+    'insertion',
+]
 
 def get_rand_set(min: int, max: int, size: int):
     set = numpy.random.randint(min, max, size=size)
@@ -44,7 +49,7 @@ def record_run(log, run, set_range, passes, comparisons, swaps):
     return log
 
 
-def process(repetition: int, min: int, max: int):
+def process(algos: List, repetition: int, min: int, max: int):
     start = datetime.utcnow()
 
     input = []
@@ -53,64 +58,72 @@ def process(repetition: int, min: int, max: int):
         for rep in range(repetition):
             input.append(get_rand_set(0, 10*power, 10**power))
 
-    # input.append([1, 8, 7, 6, 8, 6, 6, 3, 6, 9])
-
     log = {}
     fails = {}
 
-    print("-- -- --")
-    for index, set in enumerate(input):
-        set_length = len(set)
-        print(f"run: {index}/{len(input)}")
-        print(f"set length: {set_length}")
-        print(f"min magnitude: {min}")
-        print(f"max magnitude: {max}")
-        # print(set) # DBG VISIBILITY ONLY
-        start_bucket = deepcopy(set)
-        sort_start = datetime.utcnow()
-        bucket, passes, comparisons, swaps = crystal_sort(start_bucket)
-        sort_end = datetime.utcnow()
-        # print(bucket) # DBG VISIBILITY ONLY
-        # DBG TEST FAIL
-        # Uncomment the next row to replace the sorted buket with the unsorted one
-        # .. in order to test reposrting of failed sorts
-        # bucket = deepcopy(set)
-
-        confirmed = confirm_sort(bucket)
-        print(f"confirmed: {confirmed}")
-        print(f"passes: {passes}")
-        print(f"comparisons: {comparisons}")
-        print(f"swaps: {swaps}")
-        if not confirmed:
-            print(set)
-            print(bucket)
-            sorted_bucket = deepcopy(bucket)
-            sorted_bucket.sort()
-            print(sorted_bucket)
-            fails[index] = {}
-            fails[index]['range'] = set_length
-            fails[index]['passes'] = passes
-            fails[index]['swaps'] = swaps
-            fails[index]['set'] = set
-            fails[index]['processed'] = bucket
-            fails[index]['sorted'] = sorted_bucket
-            print(fails)
-        print(f"sort time: {sort_end - sort_start}")
+    for algo in algos:
+        if algo not in algo_list:
+            print(f'No {algo} algorithm')
+            continue
         print("-- -- --")
-        log = record_run(log, index, set_length, passes, comparisons, swaps)
+        print(algo)
+        for index, set in enumerate(input):
+            set_length = len(set)
+            print(f"run: {index}/{len(input)}")
+            print(f"set length: {set_length}")
+            print(f"min magnitude: {min}")
+            print(f"max magnitude: {max}")
+            # print(set) # DBG VISIBILITY ONLY
+            start_bucket = deepcopy(set)
+            sort_start = datetime.utcnow()
+            if algo == 'crystal':
+                bucket, passes, comparisons, swaps = crystal_sort(start_bucket)
+            elif algo == 'insertion':
+                bucket, passes, comparisons, swaps = insertion_sort(start_bucket)
 
-    if len(fails) > 0:
-        with open('fails.log', 'w') as logfile:
-            content = json.dumps(fails)
+            sort_end = datetime.utcnow()
+            # print(bucket) # DBG VISIBILITY ONLY
+
+            # DBG TEST FAIL
+            # Uncomment the next row to replace the sorted buket with the unsorted one
+            # .. in order to test reposrting of failed sorts
+            # bucket = deepcopy(set)
+
+            confirmed = confirm_sort(bucket)
+            print(f"confirmed: {confirmed}")
+            print(f"passes: {passes}")
+            print(f"comparisons: {comparisons}")
+            print(f"swaps: {swaps}")
+            if not confirmed:
+                print(set)
+                print(bucket)
+                sorted_bucket = deepcopy(bucket)
+                sorted_bucket.sort()
+                print(sorted_bucket)
+                fails[index] = {}
+                fails[index]['range'] = set_length
+                fails[index]['passes'] = passes
+                fails[index]['swaps'] = swaps
+                fails[index]['set'] = set
+                fails[index]['processed'] = bucket
+                fails[index]['sorted'] = sorted_bucket
+                print(fails)
+            print(f"sort time: {sort_end - sort_start}")
+            print("-- -- --")
+            log = record_run(log, index, set_length, passes, comparisons, swaps)
+
+        if len(fails) > 0:
+            with open('fails.log', 'w') as logfile:
+                content = json.dumps(fails)
+                logfile.write(content)
+
+        end = datetime.utcnow()
+        print(f"Total time: {end - start}")
+
+        with open('log.log', 'w') as logfile:
+            content = json.dumps(log)
             logfile.write(content)
-
-    end = datetime.utcnow()
-    print(f"Total time: {end - start}")
-
-    with open('log.log', 'w') as logfile:
-        content = json.dumps(log)
-        logfile.write(content)
 
 
 if __name__ == '__main__':
-    process(10, 1, 4)
+    process(['crystal'], 100, 1, 4)
