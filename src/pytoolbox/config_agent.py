3d2b23e675ce774d80a60ae3d5ebad55164eb408
Angel Slavchev <hugolabs@gmail.com>
2023-09-18 12:50:15 +0300

    Update ConfigAgent

--- src/pytoolbox/config_agent.py
+++ src/pytoolbox/config_agent.py
@@ -2,10 +2,6 @@ from configparser import ConfigParser
 import toml
 
 
-class ConfigCategory():
-    def __init__(self, category_name: str):
-        self.name = category_name
-
 class ConfigAgent():
     """
         A class used to load a timl config file
@@ -21,7 +17,7 @@ class ConfigAgent():
             full file path for the config file
         config : ConfigParser
             a dictionary of values, as read with toml
-        
+
         [loaded_parameters] : [variable]
             parameters loaded from the config file
 
@@ -31,8 +27,12 @@ class ConfigAgent():
             Returns the dictionary as read with toml
         """
 
-    def __init__(self, 
-                 config_path: str, 
+    class ConfigCategory():
+        def __init__(self, category_name: str):
+            self.name = category_name
+
+    def __init__(self,
+                 config_path: str,
                  category_prefix: str = ''):
         """
         Parameters
@@ -45,7 +45,6 @@ class ConfigAgent():
         self.config_path = config_path
         self._read_data(category_prefix)
 
-
     def get_config(self) -> ConfigParser:
         return self.config
 
@@ -53,8 +52,8 @@ class ConfigAgent():
         self.config = toml.load(self.config_path)
         for section in self.config.items():
             section_name = category_prefix + '_' + section[0] if category_prefix != '' else section[0]
-            new_section = ConfigCategory(section_name)
+            new_section = self.ConfigCategory(section_name)
             for parameter in section[1]:
-                    parameter_value = section[1][parameter]
-                    setattr(new_section, parameter, parameter_value)
+                parameter_value = section[1][parameter]
+                setattr(new_section, parameter, parameter_value)
             setattr(self, section_name, new_section)
